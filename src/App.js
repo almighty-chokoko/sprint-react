cd8fe2b209c36ec602ac48c6656cea19ba9761cb
vyacheslav kovalev <lunatikspb@gmail.com>
2018-12-15 02:51:35 +0300

    print fullname

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Hello=(word)=>{
+  const phrase = word;
+  return (
+    <h1>Hello {phrase}</h1>
+  )
+}
+
+const PrintFullName=(props)=>{
+  return (
+    <div>
+      <h1>My name {props.name}, surname - {props.surname}</h1>
+      <a href={`http://${props.link}`}>Link an my profile</a>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +36,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <main>
+          <PrintFullName name="Ivan" surname="Grechkov" link="vk.com/1"/>
+        </main>
       </div>
     );
   }
