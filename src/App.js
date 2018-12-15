ee8139506f415b216f350a01934f3d6c83f83e0a
vyacheslav kovalev <lunatikspb@gmail.com>
2018-12-15 03:38:22 +0300

    add styled-components and draft import bs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
+
 const Hello=(word)=>{
   const phrase = word;
   return (
